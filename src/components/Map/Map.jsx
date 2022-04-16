b073c70240f790bb54058d2c40ed579561e4561e
Simron <simron0707@gmail.com>
2022-04-16 15:56:03 +0530

    redeploy

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,34 +1,52 @@
 import React from 'react';
+import Map from "mapmyindia-react";
 import GoogleMapReact from 'google-map-react';
 import {Paper, Typography, useMediaQuery} from '@material-ui/core';
 import LocationOnOutlineIcon from '@material-ui/icons/LocationOnOutlined';
 import Rating from '@material-ui/lab';
 
+
 import useStyles from './styles';
 
-const Map = ({setCoordinates, setBounds ,coordinates}) => {
+const Maps = ({setCoordinates, setBounds ,coordinates}) => {
     const classes = useStyles();
     const isMobile = useMediaQuery('(min-width:600px)');
-
     return (
-        <div className={classes.mapContainer}>
-        <GoogleMapReact
-        bootstrapURLKeys={{key:'AIzaSyC539uZlHvdZ4mP-wWAKGrVCFekmlUxbLs'}}
-        defaultCenter={coordinates}
-        center={coordinates}
-        defaultZoom={14}
-        margin={[50,50,50,50]}
-        options={''}
-        onChange={''}
-        onChildClick={(e)=>{
-            setCoordinates({ lat: e.center.lat, lng: e.center.lng });
-            setBounds({sw: e.marginBounds.sw, ne: e.marginBounds.ne  })
-        }}
-        >
+        <Map
+    markers={[
+        {
+            position: [18.5314, 73.845],
+            draggable: true,
+            title: "Marker title",
+            onClick: e => {
+                console.log("clicked ");
+            },
+            onDragend: e => {
+                console.log("dragged");
+            }
+        }
+    ]}
+    />
+   )
+    // return (
+    //     <div className={classes.mapContainer}>
+    //     <GoogleMapReact
+    //     bootstrapURLKeys={{key:'AIzaSyC539uZlHvdZ4mP-wWAKGrVCFekmlUxbLs'}}
+    //     defaultCenter={coordinates}
+    //     center={coordinates}
+    //     defaultZoom={14}
+    //     margin={[50,50,50,50]}
+    //     options={''}
+    //     onChange={''}
+    //     onChildClick={(e)=>{
+    //         setCoordinates({ lat: e.center.lat, lng: e.center.lng });
+    //         setBounds({sw: e.marginBounds.sw, ne: e.marginBounds.ne  })
+    //     }}
+    //     >
         
-        </GoogleMapReact>
-        </div>
-    );
+    //     </GoogleMapReact>
+    //     </div>
+    // );
 }
 
-export default Map;
+export default Maps;
